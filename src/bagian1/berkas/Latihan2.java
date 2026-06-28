d231420fba87564f5494437d2d514e10a08670a3
SanCeses <melonhijau03@gmail.com>
2026-06-28 19:10:40 +0800

    Bagian Satu Latihan Mandiri : Selesai

--- src/bagian1/berkas/Latihan2.java
+++ src/bagian1/berkas/Latihan2.java
@@ -16,7 +16,32 @@ public class Latihan2 {
         } catch (IOException e){
             System.out.println("terjadi kesalahan   : " + e.getMessage());
         }
+             System.out.println("");
+
+        // Latihan 2
+        File arsip = new File("arsip");
+        boolean berhasil = arsip.mkdir();
+        if (berhasil) {
+            System.out.println("Folder 'arsip' berhasil dibuat.");
+        } else {
+            if (arsip.exists()) {
+                System.out.println("Folder 'arsip' sudah ada sebelumnya."); // lebih jelas
+            } else {
+                System.out.println("Folder 'arsip' gagal dibuat.");
+            }
+        }
+        System.out.println("");
         
+        // Latihan 3
+        File sementara = new File("sementara.txt");
+        try {
+            sementara.createNewFile();
+            System.out.println("Sebelum dihapus, ada? " + sementara.exists());
+            sementara.delete();
+            System.out.println("Sesudah dihapus, ada? " + sementara.exists());
+        } catch (IOException e) {
+            System.out.println("Kesalahan: " + e.getMessage());
+        }
     }
     
 }
